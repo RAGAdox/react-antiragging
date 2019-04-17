ecbad08129df3683045421e7962ea428ba0c1853
RAGAdox <rishitech882@gmail.com>
2019-04-17 08:04:10 +0530

    Login Editable Bug Fixed

--- screens/Login.js
+++ screens/Login.js
@@ -30,6 +30,30 @@ class Login extends React.Component {
       token: authUser.token
     };
   }
+  didBlurSubscription = this.props.navigation.addListener(
+    "didBlur",
+    payload => {
+      this.setState({
+        username: "",
+        password: "",
+        success: "",
+        message: "",
+        token: ""
+      });
+    }
+  );
+  willFocusSubscription=this.props.navigation.addListener(
+    'didFocus',
+    payload=>{
+      if(this.editable())
+      {
+        this.setState({message:''})
+      }
+      else{
+        console.warn('editable function returns false')
+        this.setState({message:'Already Logged in'})
+      }
+    })
   async getMsgFromApi() {
     try {
       let response = await fetch(urlAPI.url);
@@ -41,6 +65,14 @@ class Login extends React.Component {
     } catch (error) {
       console.error(error);
     }
+  }
+  editable() {
+    if (authUser.username && authUser.token) {
+      /*this.setState({
+        message: "Already Logged in"
+      });*/
+      return false;
+    } else return true;
   }
   async getTokenFromAPI() {
     var details = {
@@ -65,55 +97,63 @@ class Login extends React.Component {
         },
         body: formBody
       });
-      let responseJson =await response.json();
+      let responseJson = await response.json();
       if (responseJson.success == true) {
-        
         authUser.username = details.username;
         authUser.token = responseJson.token;
         //await SecureStore.setItemAsync('secure_token',responseJson.token);
-        await AsyncStorage.setItem("secure_token", responseJson.token).then(()=>console.warn('token saved'+responseJson.token));
+        await AsyncStorage.setItem("secure_token", responseJson.token).then(
+          () => console.warn("token saved" + responseJson.token)
+        );
         await AsyncStorage.setItem("username", details.username);
         this.setState({
           //token:await SecureStore.getItemAsync('secure_token'),
           token: await AsyncStorage.getItem("secure_token"),
-          success: responseJson.success
+          success: responseJson.success,
+          message: responseJson.message
         });
       } else {
         this.setState({
           token: "Invalid",
-          success: responseJson.success
+          success: responseJson.success,
+          message: responseJson.message
         });
       }
     } catch (error) {
       console.error(error);
     }
   }
+
   render() {
-    const {navigate} = this.props.navigation;
+    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
         <Text>this is Login</Text>
         <TextInput
           style={styles.input}
           placeholder="Username"
+          value={this.state.username}
+          editable={this.editable()}
           onChangeText={username => this.setState({ username })}
         />
         <TextInput
           style={styles.input}
           placeholder="Password"
+          value={this.state.password}
+          editable={this.editable()}
           onChangeText={password => this.setState({ password })}
         />
         <Button
           title="Login"
           onPress={() => {
-            console.warn('press')
+            console.warn("press");
             this.getTokenFromAPI().then(() => {
-              console.warn('get API')
+              console.warn("get API");
               if (this.state.success) navigate("Profile");
             });
           }}
         />
-        <Text>{this.state.token}</Text>
+        <Text>{this.state.message}</Text>
       </View>
     );
   }
